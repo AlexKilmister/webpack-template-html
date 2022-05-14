5526da72c0a076eb9939123b335b6c1e57782a86
Alex <warmetal99@gmail.com>
2022-05-14 09:48:26 +0300

    add hbs

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import './assets/scss/main.scss'
 // CSS (example)
 // import './assets/css/main.css'
 
-
 import Example from './components/Example.vue'
 
 const app = createApp(Example).mount('#app2')
